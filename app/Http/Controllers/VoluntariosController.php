cbf2f46b68b1cffc28ca92a828b9b230da549318
Inzios <62076519+Inzios@users.noreply.github.com>
2020-11-08 15:21:45 -0600

    voluntarios

---
+++ app/Http/Controllers/VoluntariosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\voluntarios;
+use Illuminate\Http\Request;
+
+class VoluntariosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\voluntarios  $voluntarios
+     * @return \Illuminate\Http\Response
+     */
+    public function show(voluntarios $voluntarios)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\voluntarios  $voluntarios
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(voluntarios $voluntarios)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\voluntarios  $voluntarios
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, voluntarios $voluntarios)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\voluntarios  $voluntarios
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(voluntarios $voluntarios)
+    {
+        //
+    }
+}
